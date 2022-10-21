ff8d17984043287a1d8e5e1aa5695d5b85708331
Venkata-Prajwal <106138692+Venkata-Prajwal@users.noreply.github.com>
2022-10-21 18:56:36 +0530

    latest

--- Prajwal Internship Task/task-be/config/dbConfig.js
+++ Prajwal Internship Task/task-be/config/dbConfig.js
@@ -1,5 +1,5 @@
 const mongodb=require('mongodb');
 const dbName='loginApp';
-const dbUrl=`mongodb+srv://venkata:Re320Re320@userdeatils.senstcf.mongodb.net/test${dbName}`  
+const dbUrl=`mongodb+srv://venkata:Re320Re320@userdeatils.senstcf.mongodb.net/${dbName}`  
 
 module.exports={dbName,dbUrl,mongodb}
